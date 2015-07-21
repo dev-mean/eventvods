5c25d27393b1897ae1426f74ea61d6791c8995e2
simon--poole <simon__poole@hotmail.com>
2015-07-21 01:27:15 +0100

    Templates -> Jade. Gitignore+ server/config/config

--- src/server/server.js
+++ src/server/server.js
@@ -7,14 +7,17 @@
     var bodyParser = require('body-parser');
     
 //server config
+	app.set('env', 'development');
     var config = require('./config/config');
-    mongoose.connect(config.databaseUrl);
     app.use(bodyParser.urlencoded({'extended':'true'}));
     app.use(bodyParser.json());
     app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+	mongoose.connect(config.databaseUrl, function(err){
+		console.log("Connected to mongodb");
+	});
     
     //templating
-    app.set('view engine', 'ejs');
+    app.set('view engine', 'jade');
     
 //routes
     require('./routes/routes')(app);
@@ -23,8 +26,22 @@
     app.use('/img', express.static(path.join(__dirname, '..', 'public/img')));
     app.use('/fonts', express.static(path.join(__dirname, '..', 'public/fonts')));
     
+
+// development error handler
+// will print stacktrace
+	if (app.get('env') === 'development') {
+	  app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		/* res.render('error', {
+		  message: err.message,
+		  error: err
+		});*/
+	  });
+	}
 //listens
     var port = config.port;
+	var db = config.databaseUrl;
+	var ip = config.ip;
     app.listen(port);
-    console.log('App listening on ' + port);
-    console.log('mongodb://' + config.ip + ':' + port);
+    console.log('App listening on ' + ip + ":" + port);
+    console.log('Database: ' +db);
