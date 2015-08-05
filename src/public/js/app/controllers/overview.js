a5ef92cd31a3e02238030e980aa861155cf87867
Simon Poole <simon__poole@hotmail.com>
2015-08-05 23:01:21 +0100

    Overview page events sections complete

--- src/public/js/app/controllers/overview.js
+++ src/public/js/app/controllers/overview.js
@@ -1,7 +1,7 @@
 angular.module('overviewController', [])
 	.controller('overviewDisplay', function($http){
 		var vm = this;
-		$http.get('api/overview').success(function(response){
+		$http.get('/api/overview').success(function(response){
 			vm.upcoming = response.upcoming;
 			vm.ongoing = response.ongoing;
 			vm.recent = response.recent;
