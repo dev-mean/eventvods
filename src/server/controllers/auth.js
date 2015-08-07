631868a18782ea667450841d4f9722c4c5be2a76
simon--poole <simon__poole@hotmail.com>
2015-08-07 18:08:23 +0100

    Typo & minor fixes

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -1,27 +1,32 @@
-module.exports = function(privRequired){
+module.exports = function(rightsNeeded){
 	return function(req, res, next) {
 		if (!req.isAuthenticated || !req.isAuthenticated()) {
 			if (req.session) {
 			  req.session.returnTo = req.originalUrl || req.url;
 			}
-		  return res.redirect('/login');
+		  return res.redirect('/users/login');
 		}
-		else if(req.user.priv >= privRequired)
+		else if(req.user.userRights >= rightsNeeded)
 			next();
 		else {
 			//We need an actual user error page to display to user
 			var err = new Error('Insufficient Permissions');
-	  		err.status = 401;
-	  		next(err);
+		  	err.status = 403;
+		  	next(err);
 		}
   	}
 }
-module.exports.checkPriv = function(req, res, next, privRequired, callback){
-	if( typeof req.user !== undefined && req.user.priv >= privRequired)
-		callback(req, res, next);
-	else {
+module.exports.checkPriv = function(req, res, next, rightsNeeded, callback){
+	if( typeof req.user !== undefined && req.user.userRights >= rightsNeeded)
+		callback(null, req, res, next);
+	else if(typeof req.user !== undefined) {
 		var err = new Error('Insufficient Permissions');
+	  	err.status = 403;
+	  	callback(err);
+	}
+	else {
+		var err = new Error('Not authenticated');
 	  	err.status = 401;
-	  	next(err);
+	  	callback(err);
 	}
 }
