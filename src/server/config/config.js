9cfe2ac070a766a7b39f119d733db453e9871f0f
Simon Poole <simon__poole@hotmail.com>
2015-09-10 20:40:23 +0100

    File upload form completed + qol changes

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    databaseUrl : 'mongodb://simon:password@ds031627.mongolab.com:31627/eventvods',
+    databaseUrl : 'ds031627.mongolab.com:31627/eventvods',
     port : process.env.PORT || 3000,
     ip : null,
     secret : 'eventvods_dev'
