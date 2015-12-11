8f18e2cdd0dd2d395758f0ae2179172b257bc718
Nick Kane <nkanedev@gmail.com>
2015-12-11 09:56:44 -0500

    Added resource for events and scrapped singleton service for the time being in order to get stuff running - will need to be refactored at a later time. Service for events created.

---
+++ src/public/js/app/resources/eventResource.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('eventApp').factory('eventResource', [
+    '$resource', 'eventConstants',
+    function($resource, eventConstants) {
+      return {
+        getEvents: $resource(eventConstants.baseUri + 'events'),
+        getEvent: $resource(eventConstants.baseUri + 'event/:eventId', { }, {
+          'get': {
+            method: 'GET',
+            params: { eventId: '@eventId' }
+          }
+        }),
+        createEvent: $resource(eventConstants.baseUri + 'events'),
+        updateEvent: $resource(eventConstants.baseUri + 'event/:eventId', { },{
+          'save': {
+            method: 'POST',
+            params: { eventId: '@eventId' }
+          }
+        }),
+        deleteEvent: $resource(eventConstants.baseUri + 'events/:eventId', { }, {
+          'delete': {
+            method: 'DELETE',
+            params: { eventId: '@eventId' }
+          }
+        })
+      }
+    }
+  ]);
+}());
---
+++ src/public/js/app/services/eventPageService.js
@@ -0,0 +1,39 @@
+(function() {
+	'use strict';
+
+	angular.module('eventApp').service('eventPageService', [
+		'eventResource',
+		function(eventResource) {
+			var service = this;
+			service.getEvents = eventResource.getEvents.query;
+			service.getEvent = eventResource.getEvent.get;
+			service.createEvent = eventResource.createEvent.save;
+			service.updateEvent = eventResource.updateEvent.save;
+			service.deleteEvent = eventResource.deleteEvent.delete;
+		}
+	]);
+}());
+
+/*
+angular.module('eventService', [])
+	.factory('Events', function($http) {
+		return {
+			get: function() {
+				return $http.get('/api/events');
+			},
+			getSingle: function(id) {
+				return $http.get('/api/event/' + id);
+			},
+			create: function(eventData) {
+				return $http.post('/api/events', eventData);
+			},
+			update: function(id, eventData) {
+				return $http.put('/api/event/' + id, eventData);
+			},
+			delete: function(id) {
+				return $http.delete('/api/events/' + id);
+			}
+		};
+	});
+angular.module('eventControllers', ['eventService']);
+*/
