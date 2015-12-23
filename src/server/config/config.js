1fb5ea6060fa2a2e3554f4aab523dcf41eff2ec2
Nick <kaneslaughter@gmail.com>
2015-12-23 14:16:39 -0500

    changing server information

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	databaseUrl: 'nkane:password@ds031627.mongolab.com:31627/eventvods',
 	port: process.env.PORT || 5000,
-	ip: 'https://stormy-earth-6103.herokuapp.com/',
+	ip: null,
 	secret: 'eventvods_dev'
 };
