0adaa1631d9b3c1fa213f92980102735b12f8df5
Martin Lindgren <martinmarklind@hotmail.com>
2015-12-26 13:01:12 +0100

    Typo after merge conflict

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -68,6 +68,7 @@ router.get('/data', auth.updater(), function (req, res) {
 
 router.get('/data/new/:type', auth.updater(), function(req, res) {
     res.render('data/form/' + req.params.type, {});
+});
     
 router.get('/data/new', auth.updater(), function (req, res) {
 	res.render('data/form/teams', {});
