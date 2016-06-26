4a2145625a74123064a65cf4bef20a8a713780a1
Simon Poole <simon__poole@hotmail.com>
2016-06-26 23:38:11 +0100

    Pause slider on hover, scale up for larger screens

--- public/js/frontend/index.js
+++ public/js/frontend/index.js
@@ -1,22 +1,25 @@
 (function($) {
+    $.fn.mouseIsOver = function() {
+        return $(this).parent().find($(this).selector + ":hover").length > 0;
+    };
     $.fn.evSlider = function(opts) {
         var defaults = {
             delay: 10000,
-            index: 0,
+            startIndex: 0,
         }
         var settings = $.extend(defaults, opts);
-        this.find('.image').eq(settings.index).addClass('active');
-        var iterate = function(){
-        	var current, next, siblings;
-        	current = this.find('.active');
-        	next = current.next('.image');
-        	siblings = current.siblings('.image');
-        	if(next.length !== 0 || siblings.length !== 0)
-        		current.removeClass('active');
-        	if(next.length > 0)
-        		next.addClass('active');
-        	else
-        		siblings.eq(0).addClass('active');
+        this.find('.image').eq(settings.startIndex).addClass('active');
+        var iterate = function() {
+            var current, next, siblings;
+            current = this.find('.active');
+            var hover = current.find('.overlay').first().mouseIsOver();
+            if(!hover){
+	            next = current.next('.image');
+	            siblings = current.siblings('.image');
+	            if (next.length !== 0 || siblings.length !== 0) current.removeClass('active');
+	            if (next.length > 0) next.addClass('active');
+	            else siblings.eq(0).addClass('active');
+	        }
         }
         setInterval($.proxy(iterate, this), settings.delay);
         return this;
