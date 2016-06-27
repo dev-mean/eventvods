807052bce58f213caf48a56a0586d09ace8e9f26
Simon Poole <simon__poole@hotmail.com>
2016-06-27 01:33:11 +0100

    Fixed phantom slide bug and reset timer on forced change

--- public/js/frontend/index.js
+++ public/js/frontend/index.js
@@ -10,27 +10,32 @@
             el: this
         }
         var settings = $.extend(defaults, opts);
+        // Initialise stuff
         this.find('.images').after('<div class="indicators"></div>');
         for(var i = 0; i < settings.max; i++)
         	this.find('.indicators').append('<span class="indicator" data-index="'+i+'">&nbsp;</span>');
+        this.find('.indicator').eq(settings.index).addClass('active');
+        this.find('.image').eq(settings.index++).addClass('active');
+
+        //Continued functions
         $('.indicator').click(function(el){
         	settings.index = $(this).attr('data-index');
         	$('.active').removeClass('active');
         	settings.el.find('.indicator').eq(settings.index).addClass('active');
         	settings.el.find('.image').eq(settings.index++).addClass('active');
-        })
-        this.find('.indicator').eq(settings.index).addClass('active');
-        this.find('.image').eq(settings.index++).addClass('active');
+        	clearInterval(settings.interval);
+        	settings.interval = setInterval($.proxy(iterate, settings.el), settings.delay);
+        });
         var iterate = function() {
+        	if(settings.index == settings.max) settings.index = 0;
             var hover = this.find('.image.active').find('.overlay').first().mouseIsOver();
             if(!hover){
             	$('.active').removeClass('active');
             	this.find('.indicator').eq(settings.index).addClass('active');
             	this.find('.image').eq(settings.index++).addClass('active');
-            	if(settings.index == settings.max) settings.index = 0;
 	        }
         }
-        setInterval($.proxy(iterate, this), settings.delay);
+        settings.interval = setInterval($.proxy(iterate, this), settings.delay);
         return this;
     };
 }(jQuery));
