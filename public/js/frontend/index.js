0fbbf7d9f22a8796ed45184a5906f3040719b5b0
Simon Poole <simon__poole@hotmail.com>
2016-06-27 01:24:35 +0100

    Slide indicators

--- public/js/frontend/index.js
+++ public/js/frontend/index.js
@@ -5,20 +5,29 @@
     $.fn.evSlider = function(opts) {
         var defaults = {
             delay: 10000,
-            startIndex: 0,
+            index: 0,
+            max: this.find('.image').length,
+            el: this
         }
         var settings = $.extend(defaults, opts);
-        this.find('.image').eq(settings.startIndex).addClass('active');
+        this.find('.images').after('<div class="indicators"></div>');
+        for(var i = 0; i < settings.max; i++)
+        	this.find('.indicators').append('<span class="indicator" data-index="'+i+'">&nbsp;</span>');
+        $('.indicator').click(function(el){
+        	settings.index = $(this).attr('data-index');
+        	$('.active').removeClass('active');
+        	settings.el.find('.indicator').eq(settings.index).addClass('active');
+        	settings.el.find('.image').eq(settings.index++).addClass('active');
+        })
+        this.find('.indicator').eq(settings.index).addClass('active');
+        this.find('.image').eq(settings.index++).addClass('active');
         var iterate = function() {
-            var current, next, siblings;
-            current = this.find('.active');
-            var hover = current.find('.overlay').first().mouseIsOver();
+            var hover = this.find('.image.active').find('.overlay').first().mouseIsOver();
             if(!hover){
-	            next = current.next('.image');
-	            siblings = current.siblings('.image');
-	            if (next.length !== 0 || siblings.length !== 0) current.removeClass('active');
-	            if (next.length > 0) next.addClass('active');
-	            else siblings.eq(0).addClass('active');
+            	$('.active').removeClass('active');
+            	this.find('.indicator').eq(settings.index).addClass('active');
+            	this.find('.image').eq(settings.index++).addClass('active');
+            	if(settings.index == settings.max) settings.index = 0;
 	        }
         }
         setInterval($.proxy(iterate, this), settings.delay);
