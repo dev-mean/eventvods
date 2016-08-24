b17388fd76e6244a5e3b69b6f9f4c40bfd330dd8
Simon Poole <simon__poole@hotmail.com>
2016-08-24 09:17:14 +0100

    Home page prototype

--- public/js/frontend/controllers/PageController.js
+++ public/js/frontend/controllers/PageController.js
@@ -69,7 +69,7 @@
 					else if (vm.session.following.tournaments.indexOf(id) > -1) return true;
 					else return false;
 				}
-				vm.toggleFollow = function(id, type) {
+				vm.toggleFollow = function(id, type, name) {
 					if (vm.session == false || vm.session == null) return $location.path('/login');
 					var index = vm.session.following[type].indexOf(id);
 					if (index > -1)
