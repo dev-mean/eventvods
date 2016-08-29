faebf9c8f431d072940cf7032338febd8dd5e681
Simon Poole <simon__poole@hotmail.com>
2016-08-29 20:23:42 +0100

    Fix responsive cookie disclaimer + image caching

--- app/controllers/aws.js
+++ app/controllers/aws.js
@@ -8,6 +8,9 @@ var file_type = require( 'file-type' );
 var Intimidate = require( 'intimidate' );
 var config = require( '../../config/config.js' );
 var aws = require( 'aws-sdk' );
+
+const tmp = path.resolve(__dirname, '../../tmp');
+
 var uploader = new Intimidate( {
     key: config.aws.key,
     secret: config.aws.secret,
@@ -19,7 +22,7 @@ aws.config.update( {
 } );
 var S3 = new aws.S3();
 
-function handleImage( fileData ) {
+function handleImage( fileData, process, processFunc  ) {
     var $promise = Q.defer();
     if ( typeof fileData.image == "undefined" || typeof fileData.image === "string" || fileData.image == null ) $promise.resolve( fileData );
     else {
@@ -27,18 +30,27 @@ function handleImage( fileData ) {
         var data = new Buffer( fileData.image.data, 'base64' );
         var type = file_type( data );
         if ( type.mime.match( 'image' ) ) {
-            var key;
-            if ( fileData.image.changed && fileData.image.oldURL.match(config.aws.cdn) ) key = fileData.image.oldURL.replace( config.aws.cdn, "" );
-            else key = fileData.field + "/" + id.generate() + "/" + id.generate() + "." + type.ext;
-            uploader.uploadBuffer( data, {
-                'Content-Type': type.mime,
-                'x-amz-acl': 'public-read'
-            }, key, function( err, res ) {
-                if ( err ) $promise.reject( "S3 upload error: " + err.message );
-                fileData.image = config.aws.cdn + key;
-                $promise.resolve( fileData );
-            } );
-        } else $promise.reject( "Invalid or corrupt image provided for field " + fileData.field );
+            var filename = id.generate() + id.generate() + "." + type.ext;
+            var key = fileData.field + "/" + filename;
+			if(process)
+				fs.writeFile(path.resolve(tmp, filename), data, function(err){
+					if(err) return $promise.reject(err);
+					fileData.image = "wip";
+					$promise.resolve(fileData);
+				});
+			else
+				uploader.uploadBuffer( data, {
+					'Content-Type': type.mime,
+					'x-amz-acl': 'public-read'
+				}, key, function( err ) {
+					if ( err ) $promise.reject( "S3 upload error: " + err.message );
+					deleteImage(fileData.image.oldURL)
+						.then(function(){
+							fileData.image = config.aws.cdn + key;
+							$promise.resolve( fileData );
+						})
+				} );
+        } else $promise.reject( "Invalid or corrupt image provided for image: " + fileData.field );
     };
     return $promise.promise;
 }
@@ -65,7 +77,7 @@ module.exports.handleUpload = function( fields ) {
             } );
     }
 }
-module.exports.deleteImage = function( key ) {
+var deleteImage = function( key ) {
     var $promise = Q.defer();
     if ( typeof key === "undefined" || key == null) $promise.resolve();
     else {
@@ -80,3 +92,4 @@ module.exports.deleteImage = function( key ) {
     }
     return $promise.promise;
 }
+module.exports.deleteImage = deleteImage;
