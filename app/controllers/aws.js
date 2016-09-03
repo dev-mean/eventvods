c06568ada545e069e6ce424f221fb521c464090d
Simon Poole <simon__poole@hotmail.com>
2016-09-03 12:45:08 +0100

    Auto-resize width before cropping header

--- app/controllers/aws.js
+++ app/controllers/aws.js
@@ -1,6 +1,3 @@
-var fs = require('fs');
-var path = require('path');
-var filetype = require('file-type');
 var Indicative = require('indicative');
 var Q = require('q');
 var id = require('shortid');
@@ -9,8 +6,6 @@ var Intimidate = require('intimidate');
 var config = require('../../config/config.js');
 var aws = require('aws-sdk');
 var gm = require('gm');
-
-const tmp = path.resolve(__dirname, '../../tmp');
 
 var uploader = new Intimidate({
 	key: config.aws.key,
@@ -27,6 +22,7 @@ function jpegify(opts){
 	var $promise = Q.defer();
 	gm(opts.data)
 		.setFormat("jpg")
+		.quality(95)
 		.toBuffer(function(err, res) {
 			if (err) $promise.reject(err);
 			opts.data = res;
@@ -35,14 +31,26 @@ function jpegify(opts){
 		})
 	return $promise.promise;
 }
-
+function headerCrop(opts){
+	var $promise = Q.defer();
+	console.log(opts.data);
+	gm(opts.data)
+		.resize(2560)
+		.crop(2560, 600, 0, 0)
+		.toBuffer(function(err, res) {
+			if (err) $promise.reject(err);
+			opts.data = res;
+			$promise.resolve(opts);
+		})
+	return $promise.promise;
+}
 function uploadImageBuffer(opts) {
 	var $promise = Q.defer();
 	uploader.uploadBuffer(opts.data, {
 			'Content-Type': opts.type,
 			'x-amz-acl': 'public-read'
 		},
-		opts.key,
+		opts.key + "." + opts.ext,
 		function(err) {
 			if (err) $promise.reject(err);
 			else $promise.resolve(opts);
@@ -52,18 +60,32 @@ function uploadImageBuffer(opts) {
 
 function headerBlur(opts) {
 	var $promise = Q.defer();
-	gm(opts.data).size(function(err, value) {
-		var width = value.width;
-		gm(opts.data)
-			.crop(width, 100, 0, 0)
-			.blur(25, 5)
-			.toBuffer(function(err, res) {
-				if (err) $promise.reject(err);
-				opts.data = res;
-				opts.key = opts.blur_key;
-				$promise.resolve(opts);
-			})
-	});
+	gm(opts.data)
+		.crop(opts.width, 100, 0, 0)
+		.blur(25, 5)
+		.toBuffer(function(err, res) {
+			if (err) $promise.reject(err);
+			opts.data = res;
+			opts.key = opts.blur_key;
+			$promise.resolve(opts);
+		})
+	return $promise.promise;
+}
+
+var deleteImage = function(key) {
+	console.log('Deleting: '+key);
+	var $promise = Q.defer();
+	if (typeof key === "undefined" || key == null) $promise.resolve();
+	else {
+		key = key.replace(config.aws.cdn, "");
+		S3.deleteObject({
+			"Bucket": config.aws.bucket,
+			"Key": key
+		}, function(err, data) {
+			if (err) $promise.reject(err);
+			else $promise.resolve();
+		});
+	}
 	return $promise.promise;
 }
 
@@ -85,23 +107,25 @@ function handleImage(fileData, process) {
 		var type = file_type(data);
 		if (type.mime.match('image')) {
 			var filename = id.generate() + id.generate();
-			var key = fileData.field + "/" + filename + "." + type.ext;
+			var key = fileData.field + "/" + filename;
 			if (process) {
 				var opts ={
 					data: data,
 					key: key,
-					blur_key: fileData.field + "/" + filename + "_b." + type.ext,
+					blur_key: fileData.field + "/" + filename + "_b",
+					ext: 'jpg',
 					oldURL: fileData.image.oldURL,
-					oldBlurURL: (typeof fileData.image.oldURL === "string") ? fileData.image.oldURL.replace(".", "_b.") : null
+					oldBlurURL: (typeof fileData.image.oldURL === "string") ? fileData.image.oldURL.replace(".jpg", "_b.jpg") : null
 				}
 				jpegify(opts)
+					.then(headerCrop)
 					.then(uploadImageBuffer)
 					.then(headerBlur)
 					.then(uploadImageBuffer)
 					.then(deleteOldImages)
 					.then(function() {
-						fileData.image = config.aws.cdn + key;
-						fileData.image_blur = config.aws.cdn + opts.blur_key;
+						fileData.image = config.aws.cdn + key + ".jpg";
+						fileData.image_blur = config.aws.cdn + opts.blur_key + ".jpg";
 						$promise.resolve(fileData);
 					})
 					.catch(function(err) {
@@ -111,13 +135,14 @@ function handleImage(fileData, process) {
 				uploadImageBuffer({
 					data: data,
 					key: key,
+					ext: type.ext,
 					type: type.mime,
 					oldURL: fileData.image.oldURL,
 					oldBlurURL: null
 				})
 				.then(deleteOldImages)
 				.then(function() {
-					fileData.image = config.aws.cdn + key;
+					fileData.image = config.aws.cdn + key + type.ext;
 					$promise.resolve(fileData);
 				})
 		} else $promise.reject("Invalid or corrupt image provided for image: " + fileData.field);
@@ -148,20 +173,5 @@ module.exports.handleUpload = function(fields) {
 				next(err);
 			});
 	}
-}
-var deleteImage = function(key) {
-	var $promise = Q.defer();
-	if (typeof key === "undefined" || key == null) $promise.resolve();
-	else {
-		key = key.replace(config.aws.cdn, "");
-		S3.deleteObject({
-			"Bucket": config.aws.bucket,
-			"Key": key
-		}, function(err, data) {
-			if (err) $promise.reject(err);
-			else $promise.resolve();
-		});
-	}
-	return $promise.promise;
 }
 module.exports.deleteImage = deleteImage;
