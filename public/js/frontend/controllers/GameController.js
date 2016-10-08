93a4aec1ef0ec0f284feed9bc0e487fef30bfb12
Simon Poole <simon__poole@hotmail.com>
2016-10-08 13:00:19 +0100

    Fix $timeout RefErr

--- public/js/frontend/controllers/GameController.js
+++ public/js/frontend/controllers/GameController.js
@@ -1,14 +1,14 @@
 (function() {
 	'use strict';
 	angular.module('eventvods')
-		.controller('GameController', ['$routeParams', '$http', 'API_BASE_URL', function($routeParams, $http, API) {
+		.controller('GameController', ['$routeParams', '$http', 'API_BASE_URL', '$timeout', function($routeParams, $http, API, $timeout) {
 			var vm = this;
 			vm.data = {};
 			var slug = $routeParams.slug;
 			$http.get(API + '/games/slug/'+slug)
 				.then(function(res){
 					vm.data = res.data;
-					$('.evSlider .image, .contents .details-toggle').addClass('loaded');
+					$('.evSlider .image').addClass('loaded');
 					$timeout(function(){
 						$('.load-in').addClass('loaded');
 					}, 100);
