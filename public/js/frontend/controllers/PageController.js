f7b19fe4fe4906d4b9e27c013a060748571ff161
Simon Poole <simon.poole@students.plymouth.ac.uk>
2016-11-18 18:52:16 +0000

    Update player play/pause to work with clicking video

--- public/js/frontend/controllers/PageController.js
+++ public/js/frontend/controllers/PageController.js
@@ -1,94 +1,101 @@
 (function() {
-	'use strict';
-	angular.module('eventvods')
-		.factory('NavService', ['$http', 'API_BASE_URL', function($http, API_BASE_URL) {
-			return {
-				get: function() {
-					return $http.get(API_BASE_URL + '/featured');
-				}
-			};
-		}])
-		.controller('PageController', ['SessionManager', '$rootScope', '$timeout', '$cookies', '$window', 'NavService', '$location',
-			function(SessionManager, $rootScope, $timeout, $cookies, $window, NavService, $location) {
-				var vm = this;
-				var element_images = 17;
-				vm.session = null;
-				vm.nav = null;
-				var now = new $window.Date(),
-					// this will set the expiration to 6 months
-					exp = new $window.Date(now.getFullYear(), now.getMonth() + 6, now.getDate());
-				var cookieSettings = {
-					expires: exp
-				}
-				vm.contentClass = $cookies.get('contentMode') || "light";
-				vm.cookiesAccepted = ($cookies.get('cookieDisclaimer') === "true");
-				vm.contentClassSet = function() {
-					$cookies.put('contentMode', vm.contentClass, cookieSettings);
-				};
-				vm.acceptCookies = function() {
-					$cookies.put('cookieDisclaimer', true, cookieSettings);
-				};
-				$rootScope.$on('sessionUpdate', function() {
-					vm.session = SessionManager.get();
-					vm.inlineYoutube = inlineYoutube();
-					vm.inlineTwitch = inlineTwitch();
-					$('.dropdown-button#userProfile').dropdown({
-						hover: true,
-						belowOrigin: true,
-						alignment: "left"
-					});
-				});
-
-				vm.logout = function() {
-					SessionManager.logout();
-				};
-				vm.init = function() {
-					$timeout(function(){
-						$('.evSlider.multiple').evSlider({
-						delay: 7500
-						});
-					}, 5);
-					$('.dropdown-button').dropdown({
-						hover: true,
-						belowOrigin: true,
-						alignment: "left"
-					});
-				};
-				vm.eventClass = function($index) {
-					if ($index === 0 ||
-						vm.nav.orderedEvents[$index].game.name !== vm.nav.orderedEvents[$index - 1].game.name) return "border-top";
-					else return "";
-				};
-				vm.elementClass = function($index) {
-					var img = ($index % element_images) + 1;
-					return "element-" + img;
-				}
-				vm.following = function(id) {
-					if (vm.session == false || vm.session == null || vm.session.following == null) return false;
-					else if (vm.session.following.indexOf(id) > -1) return true;
-					else return false;
-				}
-				vm.toggleFollow = function(id, type, name) {
-					if (vm.session == false || vm.session == null) return $location.path('/login');
-					var index = vm.session.following == null ? -1 : vm.session.following.indexOf(id);
-					if (index > -1)
-						vm.session.following.splice(index, 1);
-					else vm.session.following.push(id);
-					SessionManager.following(vm.session.following);
-				}
-				var inlineYoutube = function(){
-					if(vm.session == null || vm.session == false) return true;
-					else return vm.session.settings.inline.youtube;
-				}
-				var inlineTwitch = function(){
-					if(vm.session == null || vm.session == false) return true;
-					else return vm.session.settings.inline.youtube;
-				}
-				NavService.get()
-					.then(function(res) {
-						vm.nav = res.data;
-						vm.init();
-					});
-			}
-		]);
+    'use strict';
+    angular.module('eventvods')
+        .factory('NavService', ['$http', 'API_BASE_URL', function($http, API_BASE_URL) {
+            return {
+                get: function() {
+                    return $http.get(API_BASE_URL + '/featured');
+                }
+            };
+        }])
+        .controller('PageController', ['SessionManager', '$rootScope', '$timeout', '$cookies', '$window', 'NavService', '$location',
+            function(SessionManager, $rootScope, $timeout, $cookies, $window, NavService, $location) {
+                var vm = this;
+                var element_images = 17;
+                vm.session = null;
+                vm.nav = null;
+                var now = new $window.Date(),
+                    // this will set the expiration to 6 months
+                    exp = new $window.Date(now.getFullYear(), now.getMonth() + 6, now.getDate());
+                var cookieSettings = {
+                    expires: exp
+                }
+                vm.contentClass = $cookies.get('contentMode') || "light";
+                vm.cookiesAccepted = ($cookies.get('cookieDisclaimer') === "true");
+                vm.compact = ($cookies.get('compactDisplay') === "true");
+                vm.contentClassSet = function() {
+                    $cookies.put('contentMode', vm.contentClass, cookieSettings);
+                };
+                vm.acceptCookies = function() {
+                    $cookies.put('cookieDisclaimer', true, cookieSettings);
+                };
+                vm.compactSet = function(compact) {
+                    vm.compact = compact;
+                    $cookies.put('compactDisplay', compact, cookieSettings);
+                }
+                $rootScope.$on('sessionUpdate', function() {
+                    vm.session = SessionManager.get();
+                    vm.inlineYoutube = inlineYoutube();
+                    vm.inlineTwitch = inlineTwitch();
+                    $('.dropdown-button#userProfile').dropdown({
+                        hover: true,
+                        belowOrigin: true,
+                        alignment: "left"
+                    });
+                });
+                vm.log = function(obj) {
+                    console.log(obj);
+                }
+                vm.logout = function() {
+                    SessionManager.logout();
+                };
+                vm.init = function() {
+                    $timeout(function() {
+                        $('.evSlider.multiple').evSlider({
+                            delay: 7500
+                        });
+                    }, 5);
+                    $('.dropdown-button').dropdown({
+                        hover: true,
+                        belowOrigin: true,
+                        alignment: "left"
+                    });
+                };
+                vm.eventClass = function($index) {
+                    if ($index === 0 ||
+                        vm.nav.orderedEvents[$index].game.name !== vm.nav.orderedEvents[$index - 1].game.name) return "border-top";
+                    else return "";
+                };
+                vm.elementClass = function($index) {
+                    var img = ($index % element_images) + 1;
+                    return "element-" + img;
+                }
+                vm.following = function(id) {
+                    if (vm.session == false || vm.session == null || vm.session.following == null) return false;
+                    else if (vm.session.following.indexOf(id) > -1) return true;
+                    else return false;
+                }
+                vm.toggleFollow = function(id, type, name) {
+                    if (vm.session == false || vm.session == null) return $location.path('/login');
+                    var index = vm.session.following == null ? -1 : vm.session.following.indexOf(id);
+                    if (index > -1)
+                        vm.session.following.splice(index, 1);
+                    else vm.session.following.push(id);
+                    SessionManager.following(vm.session.following);
+                }
+                var inlineYoutube = function() {
+                    if (vm.session == null || vm.session == false) return true;
+                    else return vm.session.settings.inline.youtube;
+                }
+                var inlineTwitch = function() {
+                    if (vm.session == null || vm.session == false) return true;
+                    else return vm.session.settings.inline.youtube;
+                }
+                NavService.get()
+                    .then(function(res) {
+                        vm.nav = res.data;
+                        vm.init();
+                    });
+            }
+        ]);
 }());
--- public/js/frontend/controllers/PlayerController.js
+++ public/js/frontend/controllers/PlayerController.js
@@ -1,141 +1,152 @@
 (function() {
-	'use strict';
-	angular.module('eventvods')
-		.controller('PlayerController', ['$rootScope', '$timeout', 'SessionManager', function($rootScope, $timeout, SessionManager) {
-			var vm = this, player = null;
-			vm.show = false;
-			vm.match = null;
-			vm.type = null;
-			vm.loaded = true;
-			vm.playing = false;
-			vm.sectionT = null;
-			vm.moduleT = null;
-			vm.back = function(){
-				reset();
-			}
-			function reset(){
-				player.destroy();
-				player = null;
-				vm.type = null;
-				vm.match = null;
-				$('#player').empty();
-				vm.show = false;
-			}
-			function timeToSeconds(time){
-				if(typeof time === "undefined") return 0;
-				time = /((\d+)h)?((\d+)m)?((\d+)s)?/i.exec(time);
-				for(var i = 0; i < time.length; i++){
-					if(typeof time[i] === "undefined") time[i] = 0;
-				}
-				return (parseInt(time[2] * 3600) + parseInt(time[4] * 60) + parseInt(time[6]));
-			}
-			function parseTwitch(link){
-				var twitch = /http(s|):\/\/(www\.|)twitch\.tv\/(\S+)\/v\/([0-9]+)(?:\?t=(\S+)|$)/i.exec(link);
-				if(twitch == null) return false;
-				else return {
-					vid: "v"+twitch[4],
-					time: timeToSeconds(twitch[5])
-				}
-			}
-			function parseYoutube(link){
-				var yt = /http(s|):\/\/(www\.|)(youtube\.com\/watch\?v=|youtu\.be\/)([^&]+)([&\?]t=(\S+)?)?/i.exec(link);
-				if(yt == null) return false;
-				else return {
-					vid: yt[4],
-					time: timeToSeconds(yt[6])
-				}
-			}
-			vm.playYoutube = function(link, match, $event, sectionT, moduleT){
-				if(match.placeholder) return;
-				var yt = parseYoutube(link);
-				if(yt !== false) $event.preventDefault();
-				vm.show = true;
-				vm.match = match;
-				vm.type = "youtube";
-				vm.sectionT = sectionT;
-				vm.moduleT = moduleT;
-				var options = {
-					width: window.innerWidth * 0.6,
-					height: window.innerWidth * 0.6 * 9/16,
-					videoId: yt.vid,
-					playerVars: {
-						controls: 0,
-						showinfo: 0,
-						modestbranding: 1,
-						start: yt.time
-					},
-					events: {
-						onReady: function(event) {
-							event.target.playVideo();
-							vm.playing = true;
-							vm.volSlider = event.target.getVolume();
-						}
-					}
-				};
-				player = new YT.Player('player', options);
-			}
-			vm.playTwitch = function(link, match, $event, sectionT, moduleT){
-				if(match.placeholder) return;
-				var twitch = parseTwitch(link);
-				if(twitch !== false) $event.preventDefault();
-				console.log(twitch);
-				vm.loaded = false;
-				vm.show = true;
-				vm.match = match;
-				vm.type = "twitch";
-				vm.sectionT = sectionT;
-				vm.moduleT = moduleT;
-				var options = {
-					width: window.innerWidth * 0.6,
-					height: window.innerWidth * 0.6 * 9/16,
-					video: twitch.vid
-				};
-				player = new Twitch.Player("player", options);
-				function init(){
-					vm.playing = true;
-					player.seek(twitch.time);
-					player.removeEventListener(Twitch.Player.READY, init);
-					$timeout(function(){
-						vm.loaded = true;
-					}, 1);
-				}
-				player.addEventListener(Twitch.Player.READY, init);
-			}
-			vm.picks = function(){
-				if(vm.type === "twitch"){
-					var time = parseTwitch(vm.match.twitch.picksBans);
-					if(time !== false) time = time.time;
-					player.seek(time);
-				}
-				if(vm.type === "youtube"){
-					var time = parseYoutube(vm.match.youtube.picksBans);
-					if(time !== false) time = time.time;
-					player.seekTo(time);
-				}
-			}
-			vm.start = function(){
-				if(vm.type === "twitch"){
-					var time = parseTwitch(vm.match.twitch.gameStart);
-					if(time !== false) time = time.time;
-					player.seek(time);
-				}
-				if(vm.type === "youtube"){
-					var time = parseYoutube(vm.match.youtube.gameStart);
-					if(time !== false) time = time.time;
-					player.seekTo(time);
-				}
-			}
-			vm.toggle = function(){
-				if(vm.playing) player.pauseVideo();
-				else player.playVideo();
-				vm.playing = !vm.playing;
-			}
-			vm.setVol = function(vol){
-				vm.volSlider = vol;
-				player.setVolume(vol);
-			}
-			vm.skip = function(diff){
-				player.seekTo(player.getCurrentTime() + diff);
-			}
-		}]);
+    'use strict';
+    angular.module('eventvods')
+        .controller('PlayerController', ['$rootScope', '$timeout', 'SessionManager', '$scope', function($rootScope, $timeout, SessionManager, $scope) {
+            var vm = this,
+                player = null;
+            vm.show = false;
+            vm.match = null;
+            vm.type = null;
+            vm.loaded = true;
+            vm.playing = false;
+            vm.sectionT = null;
+            vm.moduleT = null;
+            vm.back = function() {
+                reset();
+            }
+
+            function reset() {
+                player.destroy();
+                player = null;
+                vm.type = null;
+                vm.match = null;
+                $('#player').empty();
+                vm.show = false;
+            }
+
+            function timeToSeconds(time) {
+                if (typeof time === "undefined") return 0;
+                time = /((\d+)h)?((\d+)m)?((\d+)s)?/i.exec(time);
+                for (var i = 0; i < time.length; i++) {
+                    if (typeof time[i] === "undefined") time[i] = 0;
+                }
+                return (parseInt(time[2] * 3600) + parseInt(time[4] * 60) + parseInt(time[6]));
+            }
+
+            function parseTwitch(link) {
+                var twitch = /http(s|):\/\/(www\.|)twitch\.tv\/(\S+)\/v\/([0-9]+)(?:\?t=(\S+)|$)/i.exec(link);
+                if (twitch == null) return false;
+                else return {
+                    vid: "v" + twitch[4],
+                    time: timeToSeconds(twitch[5])
+                }
+            }
+
+            function parseYoutube(link) {
+                var yt = /http(s|):\/\/(www\.|)(youtube\.com\/watch\?v=|youtu\.be\/)([^&]+)([&\?]t=(\S+)?)?/i.exec(link);
+                if (yt == null) return false;
+                else return {
+                    vid: yt[4],
+                    time: timeToSeconds(yt[6])
+                }
+            }
+            vm.playYoutube = function(link, match, $event, sectionT, moduleT) {
+                if (match.placeholder) return;
+                var yt = parseYoutube(link);
+                if (yt !== false) $event.preventDefault();
+                vm.show = true;
+                vm.match = match;
+                vm.type = "youtube";
+                vm.sectionT = sectionT;
+                vm.moduleT = moduleT;
+                var options = {
+                    width: window.innerWidth * 0.6,
+                    height: window.innerWidth * 0.6 * 9 / 16,
+                    videoId: yt.vid,
+                    playerVars: {
+                        controls: 0,
+                        showinfo: 0,
+                        modestbranding: 1,
+                        start: yt.time
+                    },
+                    events: {
+                        "onReady": function(event) {
+                            event.target.playVideo();
+                            vm.playing = true;
+                            vm.volSlider = event.target.getVolume();
+                        },
+                        "onStateChange": function(event) {
+                            $scope.$apply(function() {
+                                if (event.data === 1) vm.playing = true;
+                                if (event.data === 2) vm.playing = false;
+                            });
+                        }
+                    }
+                };
+                player = new YT.Player('player', options);
+            }
+            vm.playTwitch = function(link, match, $event, sectionT, moduleT) {
+                if (match.placeholder) return;
+                var twitch = parseTwitch(link);
+                if (twitch !== false) $event.preventDefault();
+                console.log(twitch);
+                vm.loaded = false;
+                vm.show = true;
+                vm.match = match;
+                vm.type = "twitch";
+                vm.sectionT = sectionT;
+                vm.moduleT = moduleT;
+                var options = {
+                    width: window.innerWidth * 0.6,
+                    height: window.innerWidth * 0.6 * 9 / 16,
+                    video: twitch.vid
+                };
+                player = new Twitch.Player("player", options);
+
+                function init() {
+                    vm.playing = true;
+                    player.seek(twitch.time);
+                    player.removeEventListener(Twitch.Player.READY, init);
+                    $timeout(function() {
+                        vm.loaded = true;
+                    }, 1);
+                }
+                player.addEventListener(Twitch.Player.READY, init);
+            }
+            vm.picks = function() {
+                if (vm.type === "twitch") {
+                    var time = parseTwitch(vm.match.twitch.picksBans);
+                    if (time !== false) time = time.time;
+                    player.seek(time);
+                }
+                if (vm.type === "youtube") {
+                    var time = parseYoutube(vm.match.youtube.picksBans);
+                    if (time !== false) time = time.time;
+                    player.seekTo(time);
+                }
+            }
+            vm.start = function() {
+                if (vm.type === "twitch") {
+                    var time = parseTwitch(vm.match.twitch.gameStart);
+                    if (time !== false) time = time.time;
+                    player.seek(time);
+                }
+                if (vm.type === "youtube") {
+                    var time = parseYoutube(vm.match.youtube.gameStart);
+                    if (time !== false) time = time.time;
+                    player.seekTo(time);
+                }
+            }
+            vm.toggle = function() {
+                if (vm.playing) player.pauseVideo();
+                else player.playVideo();
+            }
+            vm.setVol = function(vol) {
+                vm.volSlider = vol;
+                player.setVolume(vol);
+            }
+            vm.skip = function(diff) {
+                player.seekTo(player.getCurrentTime() + diff);
+            }
+        }]);
 }());
