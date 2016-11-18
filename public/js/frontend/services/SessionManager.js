6e094391f5a9cecc9b040c359c64ea763569c22e
Simon Poole <simon.poole@students.plymouth.ac.uk>
2016-11-18 19:02:21 +0000

    Remove session comments

--- public/js/frontend/services/SessionManager.js
+++ public/js/frontend/services/SessionManager.js
@@ -1,69 +1,67 @@
 (function() {
-	'use strict';
-	angular.module('eventvods')
-		.service('SessionManager', ['$http', 'API_BASE_URL', '$q', '$rootScope', function($http, URL, $q, $rootScope) {
-			var svc = this,
-				session = null;
-			svc.get = function() {
-				console.log('Current session:');
-				console.log(session);
-				return session;
-			};
-			svc.load = function() {
-				$http.get(URL + '/auth/session')
-					.then(function(res) {
-						if (res.status === 200) session = res.data;
-						else session = false;
-						$rootScope.$broadcast('sessionUpdate');
-					})
-					.catch(function() {
-						session = false;
-						$rootScope.$broadcast('sessionUpdate');
-					});
-			};
-			svc.login = function(data) {
-				var q = $q.defer();
-				$http.post(URL + '/auth/login', data)
-					.then(function(res) {
-						session = res.data;
-						$rootScope.$broadcast('sessionUpdate');
-						q.resolve();
-					})
-					.catch(function(err) {
-						session = false;
-						$rootScope.$broadcast('sessionUpdate');
-						q.reject(err.data);
-					});
-				return q.promise;
-			};
-			svc.register = function(data) {
-				var q = $q.defer();
-				$http.post(URL + '/auth/register', data)
-					.then(function(res) {
-						session = res.data;
-						$rootScope.$broadcast('sessionUpdate');
-						q.resolve();
-					})
-					.catch(function(errs) {
-						session = false;
-						$rootScope.$broadcast('sessionUpdate');
-						q.reject(errs.data);
-					});
-				return q.promise;
-			};
-			svc.logout = function() {
-				$http.get(URL + '/auth/logout')
-					.finally(function() {
-						session = false;
-						$rootScope.$broadcast('sessionUpdate');
-					});
-			};
-			svc.following = function(data) {
-				$http.post(URL + '/user/following', data, {
-					ignoreLoadingBar: true
-				})
-			}
-			$rootScope.$on('triggerSessionUpdate', svc.load);
-			svc.load();
-		}]);
+    'use strict';
+    angular.module('eventvods')
+        .service('SessionManager', ['$http', 'API_BASE_URL', '$q', '$rootScope', function($http, URL, $q, $rootScope) {
+            var svc = this,
+                session = null;
+            svc.get = function() {
+                return session;
+            };
+            svc.load = function() {
+                $http.get(URL + '/auth/session')
+                    .then(function(res) {
+                        if (res.status === 200) session = res.data;
+                        else session = false;
+                        $rootScope.$broadcast('sessionUpdate');
+                    })
+                    .catch(function() {
+                        session = false;
+                        $rootScope.$broadcast('sessionUpdate');
+                    });
+            };
+            svc.login = function(data) {
+                var q = $q.defer();
+                $http.post(URL + '/auth/login', data)
+                    .then(function(res) {
+                        session = res.data;
+                        $rootScope.$broadcast('sessionUpdate');
+                        q.resolve();
+                    })
+                    .catch(function(err) {
+                        session = false;
+                        $rootScope.$broadcast('sessionUpdate');
+                        q.reject(err.data);
+                    });
+                return q.promise;
+            };
+            svc.register = function(data) {
+                var q = $q.defer();
+                $http.post(URL + '/auth/register', data)
+                    .then(function(res) {
+                        session = res.data;
+                        $rootScope.$broadcast('sessionUpdate');
+                        q.resolve();
+                    })
+                    .catch(function(errs) {
+                        session = false;
+                        $rootScope.$broadcast('sessionUpdate');
+                        q.reject(errs.data);
+                    });
+                return q.promise;
+            };
+            svc.logout = function() {
+                $http.get(URL + '/auth/logout')
+                    .finally(function() {
+                        session = false;
+                        $rootScope.$broadcast('sessionUpdate');
+                    });
+            };
+            svc.following = function(data) {
+                $http.post(URL + '/user/following', data, {
+                    ignoreLoadingBar: true
+                })
+            }
+            $rootScope.$on('triggerSessionUpdate', svc.load);
+            svc.load();
+        }]);
 }());
